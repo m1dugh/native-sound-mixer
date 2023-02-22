6542e838e95b039a6659bd43d012461f507b4261
Xorus <jdpepi@gmail.com>
2023-02-22 22:01:55 +0100

    reformat code

--- cppsrc/win/sound-mixer.cpp
+++ cppsrc/win/sound-mixer.cpp
@@ -376,8 +376,8 @@ Napi::Value AudioSessionObject::GetMute(const Napi::CallbackInfo &info)
 
 Napi::Value AudioSessionObject::GetState(const Napi::CallbackInfo &info)
 {
-    return Napi::Number::New(
-        info.Env(), static_cast<int>(reinterpret_cast<AudioSession *>(pSession)->state()));
+    return Napi::Number::New(info.Env(),
+        static_cast<int>(reinterpret_cast<AudioSession *>(pSession)->state()));
 }
 
 void AudioSessionObject::SetMute(
